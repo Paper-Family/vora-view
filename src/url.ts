50e3d17d5d2f96a11dde04b71263f1bbebd1c1af
Lee JaeGyeom <thfpdhk1@gmail.com>
2026-02-10 18:41:50 +0900

    [Feat] API URL

---
+++ src/url.ts
@@ -0,0 +1,8 @@
+const URL = "https://vora-api.onrender.com/api/";
+const url = {
+    LOGIN: URL + "auth/login",
+    LOGOUT: URL + "auth/logout",
+    SIGNUP: URL + "auth/signup",
+    ARTICLES: URL + "articles",
+}
+export default url;
