041aadd5b3027ff02c8fce32c1cf61afc385fbad
jayLrei <thfpdhk1@gmail.com>
2026-02-13 00:14:28 +0900

    [Feat] Login token check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import HomePage from "@/page/home";
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
+
+  if (!token) {
+    redirect("/login");
+  }
+
   return <HomePage />;
 }
