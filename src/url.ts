9b35f8549537f6354af65158bb488d901290b40d
jayLrei <thfpdhk1@gmail.com>
2026-02-15 23:27:15 +0900

    [Chore] url

--- src/url.ts
+++ src/url.ts
@@ -1,8 +1,8 @@
 const URL = "https://vora-api.onrender.com/api/";
 const url = {
-    LOGIN: URL + "auth/login",
-    LOGOUT: URL + "auth/logout",
-    SIGNUP: URL + "auth/signup",
-    ARTICLES: URL + "articles",
-}
+  LOGIN: URL + "auth/login",
+  LOGOUT: URL + "auth/logout",
+  SIGNUP: URL + "auth/signup",
+  ARTICLE: URL + "article",
+};
 export default url;
